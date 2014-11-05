cc88ae8b45234a14ae66673e8030b324877ed200
Daniel Stern <daniel@danielstern.ca>
2014-11-04 17:42:43 -0800

    comin-along-great-no-broken

--- app/typescript/main.ts
+++ app/typescript/main.ts
@@ -16,11 +16,32 @@ angular.module("DemoApp",['ui.router'])
     	templateUrl:'partials/portfolio.html'
 	})
 	.state('project',{
-		url: '/project/:item',
-    	templateUrl:function($stateParams){
-    		return 'partials/portfolio/'+$stateParams.item+'.html';
+		url: '/project/:project',
+		templateUrl:'partials/project.html',
+    	controller:function($scope,portfolio,$stateParams){
+    		$scope.item = portfolio.filter(function(item){
+    			return item.shortname === $stateParams.project;
+			})[0];
+    	}		
+	})
+	.state('project.image',{
+		url: '/:image',
+		templateUrl:'partials/project-image-feature.html',
+    	controller:function($scope,portfolio,$stateParams){
+    		$scope.image = portfolio.map(function(item){
+    			return item.img;
+			}).reduce(function(prev,next,index,init){
+				next.forEach(function(_next){
+					prev.push(_next);
+				});
+				return prev;
+			},[]).filter(function(img){
+				return img.shortname === $stateParams.image;
+			})[0];
+
+			console.log("image?",$scope.image);
+
     	}
-    	// template:"An item"
 	})
 	.state('features',{
 		url: '/features',
@@ -40,7 +61,23 @@ angular.module("DemoApp",['ui.router'])
 	category:"Website",
 	shortname:"maple-a-month",
 	link: "http://mapleamonth.com",
-	img: "img/maple-a-month-1.png"
+	img: [{
+		url:"img/maple-a-month-1.png",
+		shortname:'maple-logo',
+		description:"A logo that I made in Photoshop",
+		title:'Maple a Month Logo',
+		alt:'Maple a month with a leaf behind it'
+	},{
+		url:"img/maple-a-month-2.png",
+		shortname:'how-it-works',
+		title:'some caption',
+		alt:'some alt'
+	},{
+		url:"img/maple-a-month-3.png",
+		shortname:'paypal-sales',
+		title:'some caption',
+		alt:'some alt'
+	}]
 },{
 	name:"range.css",
 	description:"HTML5 tool to create styled range inputs with CSS. Featured on css-tricks.com",
@@ -48,7 +85,14 @@ angular.module("DemoApp",['ui.router'])
 	category:"Library",
 	shortname:"range-css",
 	link: "http://danielstern.ca/range.css/#/",
-	img: "img/range-1.png"
+	img: [{
+		url:"img/maple-a-month-1.png",
+		shortname:'maple-a-month-1',
+		title:'some caption',
+		alt:'some alt'
+	}]
+
+	// img: "img/range-1.png"
 },{
 	name:"ngAudio",
 	description:"A JavaScript library used to intergrate audio elements into Angular.js projects. Was featured at Full-Stack Conf in London, England where I did a talk.",
@@ -56,5 +100,11 @@ angular.module("DemoApp",['ui.router'])
 	category:"Library",
 	shortname:"angular-audio",
 	link: "http://danielstern.github.io/ngAudio/#/",
-	img: "img/angular-audio-1.png"
+	img: [{
+		url:"img/maple-a-month-1.png",
+		shortname:'maple-a-month-1',
+		title:'some caption',
+		alt:'some alt'
+	}]
+	// img: "img/angular-audio-1.png"
 }])
